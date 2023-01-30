d894bbb051ef4f21ca901e41260889bf7363e0fe
StefenGPD <decena.stefengenoa@gmail.com>
2023-01-30 13:28:15 +0800

    Buttons Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+function container() {
+  return (
+    <div className="container">
+      <NumberField />
+      <ButtonGroup />
+    </div>
+  );
+}
+
+function NumberField() {
+  return (
+    <input type="text" placeholder="0"> </input>
+  );
+}
+
+function NumberButton(numValue = '0') {
+  return (
+    <div>{numValue}</div>
+  );
+}
+
+
+function ButtonGroup() {
+  return(
+    <div>
+      <NumberButton numValue={'7'} />
+      <NumberButton numValue={'8'} />
+      <NumberButton numValue={'9'} />
+      <NumberButton numValue={'4'} />
+      <NumberButton numValue={'5'} />
+      <NumberButton numValue={'6'} />
+      <NumberButton numValue={'1'} />
+      <NumberButton numValue={'2'} />
+      <NumberButton numValue={'3'} />
+    
+    </div>
+    
+  );
+}
+
 function App() {
   return (
     <div className="App">
@@ -22,4 +62,4 @@ function App() {
   );
 }
 
-export default App;
+export default container;
