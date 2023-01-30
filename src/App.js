408d47d74384cab96cf39ef0d5bdffc4aa08dce8
StefenGPD <decena.stefengenoa@gmail.com>
2023-01-30 13:29:25 +0800

    d

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-function container() {
-  return (
-    <div className="container">
-      <NumberField />
-      <ButtonGroup />
-    </div>
-  );
-}
-
 function NumberField() {
   return (
     <input type="text" placeholder="0"> </input>
@@ -44,20 +35,8 @@ function ButtonGroup() {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NumberField />
+      <ButtonGroup />
     </div>
   );
 }
